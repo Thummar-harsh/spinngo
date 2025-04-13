4e35b798479537b68bc5046f0100e1cf34cdc79b
thummar-harsh <tummar.harsh.7@gmail.com>
2025-04-13 11:29:45 +0530

    favicon change

--- src/App.tsx
+++ src/App.tsx
@@ -181,7 +181,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <img src="/faviconbgr.png" alt="Logo" className="h-8 w-8" />
+              <img src="/faviconbgr.png " alt="Logo" className="h-8 w-8" />
               <span className="ml-2 text-xl font-semibold text-gray-800 dark:text-white">Spin N Go</span>
             </div>
             <div className="flex items-center space-x-4">
